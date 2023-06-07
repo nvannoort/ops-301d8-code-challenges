200af0b7d5b981b9bc400cb74ac9fe29e21142fb
nvannoort <nick.vannoort@gmail.com>
2023-06-07 22:02:13 +0000

    this is for class 06

---
+++ pythonbash.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Ops301d8 Day 06
+# Author:                       Your Name: Nick Van Noort
+# Date of latest revision:      06/06/2023
+# Purpose:                      Start to learn Python
+
+# varibles
+IPresults = os.system("ip a")
+user = os.system("whoami")
+list = os.system("lshw -short")
+
+# print to screen
+print(IPresults)
+print(user)
+print(list)
+
+# end
