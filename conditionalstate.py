d4935b3123f59b3ae831095870c7084e77c25e69
nvannoort <nick.vannoort@gmail.com>
2023-06-09 23:28:35 +0000

    conditional statements

---
+++ conditionalstate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Ops301d8 Day 09
+# Author:                       Your Name: Nick Van Noort
+# Date of latest revision:      06/09/2023
+# Purpose:                      Conditional Statements in Python
+
+# Variable Declarations
+a = 5
+b = 6
+c = 10
+d = 11
+e = 1
+f = 1
+
+# if statements 
+if e == f:
+    print("e is equal to f")
+
+if a != b:
+    print("e is not equal to f")
+
+if a < b:
+    print("a is less than b")
+
+if a <= b:
+    print("a is less than or equal to b")
+
+if a > b:
+    print("a is greater than b")
+
+if a >= b:
+    print("a is greater than or equal to b")
+
+# if elif statement: a is or is not equal to b
+
+if a == b:
+    print("a is equal to b")
+elif a != b:
+    print("a is not equal to b")
+
+# elif else statement: 
+
+if c > d:
+    print("c is greater than d")
+elif c < d:
+    print("c is less than d")
+else:
+    print("c is equal to d")
+
+# done
